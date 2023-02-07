1955bca89d153c0cd3d83ede04eeb99fa9a32a8e
Robko74 <124675593+Robko74@users.noreply.github.com>
2023-02-07 17:28:36 +0100

    Aktualizovať pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    kitronik_servo_lite.setDistancePerSecond(5)
+    kitronik_servo_lite.driveBackwards(10)
+    kitronik_servo_lite.driveForwards(10)
+})
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-strip.show()
-basic.pause(5000)
+for (let index = 0; index < 4; index++) {
+    strip.setBrightness(50)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    basic.pause(500)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(500)
+}
 strip.clear()
 strip.show()
